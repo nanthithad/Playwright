15137475dcf9b4df52fc39d6b0fed6f6f9543a51
nanthithad <nanthithad@gmail.com>
2025-09-10 11:57:51 +0530

    Corrected Path

--- playwright.config.js
+++ playwright.config.js
@@ -1,16 +1,29 @@
-const { defineConfig } = require('@playwright/test');
+const { defineConfig, devices } = require('@playwright/test');
+
 module.exports = defineConfig({
-workers: process.env.CI ? 4 : undefined,
-retries: process.env.CI ? 2 : 0,
-reporter: [
-['list'],
-['html', { outputFolder: 'playwright-report', open: 'never' }],
-['junit', { outputFile: 'test-results/junit-results.xml' }],
-['allure-playwright', { outputFolder: 'allure-results' }]
-],
-use: {
-trace: 'on-first-retry',
-video: 'on-first-retry',
-screenshot: 'on',
-}
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+  reporter: [
+    ['list'],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['junit', { outputFile: 'test-results/junit-results.xml' }],
+  ],
+  use: {
+    trace: 'on-first-retry',
+    video: 'on-first-retry',
+    screenshot: 'on',
+  },
 });
+
