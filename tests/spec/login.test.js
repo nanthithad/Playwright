6ff8af89ea3d65307eaad166f64f96842738f5c5
nanthithad <nanthithad@gmail.com>
2025-09-10 12:33:45 +0530

    .

--- tests/spec/login.test.js
+++ tests/spec/login.test.js
@@ -1,8 +1,7 @@
 const { test } = require('@playwright/test');
-const { LoginPage } = require('../pages/loginpage');
+const { LoginPage } = require('../pages/loginpage.js');
 test.describe('OrangeHRM Login Scenarios', () => {
- test('should successfully log in with valid credentials', async ({ page
-}) => {
+ test('should successfully log in with valid credentials', async ({ page}) => {
  const loginPage = new LoginPage(page);
  await loginPage.goto();
  await loginPage.login('Admin', 'admin123');
